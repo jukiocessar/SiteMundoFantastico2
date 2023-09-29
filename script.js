7a61803cb360683f9cb78e6dcceebaa2117caefe
jukiocessar <132167353+jukiocessar@users.noreply.github.com>
2023-09-29 15:42:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function abrirD(){
+  document.querySelector("#Ops").open=true
+}
+function fecharD(){
+  document.querySelector("#Ops").open=false
+}
+
+
+//função de passar imagem
+//function passarImagem(){
+  //document.querySelector("#imagemprimaria").src = "imagens/IMG-20230922-WA0003.jpg"
+  
+//}
+
